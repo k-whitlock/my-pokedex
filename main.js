9a5baea4ec0f1ef5f93a073314f02b56665cbbee
Kam <Kwhitlock1217@gmail.com>
2020-08-10 18:59:10 -0700

    Added 2nd fetch to get individual pokemon data

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-async function getPokemon(){
+async function getAll(){
 
     //fetch data
     try {
@@ -6,21 +6,34 @@ async function getPokemon(){
         const data = await result.json();
         console.log(data);
 
-      // turn json array into just an array of pokemon names
-      const pokemon = data.results;
-        
-      console.log(pokemon);
-      
+        // turn json array into just an array of pokemon names
+        const list = data.results;
+        console.log(list); 
 
-  // when screen loads choose a random pokemon from const pokemon
-  let random = pokemon[Math.floor(Math.random() * pokemon.length)]; 
-      console.log(random);
-      
-  // from the random pokemon get url
-  const pokeurl = random.url;
-  console.log(pokeurl);
+     // when you open the page it chooses a random pokemon from const list
+        let random = list[Math.floor(Math.random() * list.length)]; 
+        console.log(random); 
+    
+    // from the random pokemon get url
+        const pokeurl = random.url;
+        console.log(pokeurl);
 
 
+    // fetch poke url to get details on individual pokemon
+       let fetchPokeData = await fetch (pokeurl);
+       let pokeData = await fetchPokeData.json();
+       console.log(pokeData);
+       
+    // once we get data choose what you want to show
+       const pokemon = {
+           name: pokeData.name,
+           id: pokeData.id,
+           image: pokeData.sprites['front_default'],
+    // goes through each pokemon type and puts into a single array. Then converts to a string
+           type: pokeData.types.map(t => t.type.name).join(',')
+          
+       };
+       console.log(pokemon);
         
     } catch(error) {
         alert(error);
@@ -28,4 +41,6 @@ async function getPokemon(){
     
 };
 
-getPokemon();
+
+
+getAll();
