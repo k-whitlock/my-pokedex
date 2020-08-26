c66256a1ac5de52de5af9cc2248db8cf7a309dc1
Kam <Kwhitlock1217@gmail.com>
2020-08-25 17:22:22 -0700

    Updated CSS, testing out different styles

--- main.js
+++ main.js
@@ -7,7 +7,8 @@ const pokeFirstType = document.querySelector('.poke-first-type');
 const pokeSecondType = document.querySelector('.poke-second-type');
 const nextPoke = document.querySelector('.poke-btn');
 
-
+//ADDING EVENT LISTENERS
+nextPoke.addEventListener('click', getAll);
     
 
 //get data for all pokemon
@@ -18,19 +19,52 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`)
     .then(res => res.json())
     .then(data => {
         
+    //destructing used to get data from array
     const results = data['results'];
-    
+   
 
     // when you open the page it chooses a random pokemon from const list
     let random = results[Math.floor(Math.random() * results.length)]; 
-    console.log(random); 
+  
 
 // from the random pokemon get url
     let pokeurl = random['url'];
-    console.log(pokeurl);
+    
 
 
+//fetch data for individual pokemon
+fetch(pokeurl)
+    .then(res => res.json())
+    .then(data => {
+    
+        
+    //shows data on screen 
+    pokeScreen.classList.remove('d-none');
 
+    //get the data types
+    const dataTypes = data['types'];
+    console.log(dataTypes);
+    const dataFirstType = dataTypes[0];
+    const dataSecondType = dataTypes[1];
+    pokeFirstType.textContent = dataFirstType['type']['name'];
+
+    //if there are more than 1 type remove hide class vice versa
+    if(dataSecondType) {
+        pokeSecondType.classList.remove('d-none');
+        pokeSecondType.textContent = dataSecondType['type']['name'];
+    } else {
+        pokeSecondType.classList.add('d-none');
+        pokeSecondType.textContent = '';
+    } 
+    // if there is no image put blank 
+    pokeImage.src = data['sprites']['front_default'] || "";
+
+    pokeName.textContent = data['name'];
+
+    // put # in front of number, convert to string, then add "0" in front of it if less than 100
+    pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+
+        });
 
 
     });
